241cef49126ffd8dbc0fc71f4e34004d2dcfe005
Timothy Barna <audioclimb@gmail.com>
2020-01-07 15:42:27 -0700

    first commit

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+export default class EvenAndOdd extends Component {
+    render() {
+        return(
+            <p>EvenAndOdd Component</p>
+        )
+    }
+}
